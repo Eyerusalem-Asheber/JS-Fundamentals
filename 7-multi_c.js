1cce43207082e4db677dad848dc904a49de4cd7f
Eyerusalem <eyerusalem.asheber.1@gmail.com>
2025-07-20 14:07:50 +0300

    file 7

---
+++ 7-multi_c.js
@@ -0,0 +1,8 @@
+const arg = process.argv[2];
+const x = parseInt(arg);
+console.log(isNaN(x) ? "Missing number of occurrences" : 
+let i = 0;
+  while (i < x) {
+    console.log("C is fun");
+    i++;
+); 
