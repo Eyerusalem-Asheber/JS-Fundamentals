f8269d987bb81190915f4264fa7ab9a8070404a9
Eyerusalem <eyerusalem.asheber.1@gmail.com>
2025-07-20 13:09:52 +0300

    add file 3

---
+++ 2-arguments.js
@@ -0,0 +1,10 @@
+const args = []
+if (args.length === 0) {
+    console.log("No argument");}
+    else if (args.length ===1) {
+        console.log("Argument found")}
+        else {console.log("Arguments found")
+    };
+
+// Get the number of arguments passed (excluding node and script name)
+
