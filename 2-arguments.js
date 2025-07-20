2a1f5a6aeecd7ec4f3f9d5a629d6502b028eba59
Eyerusalem <eyerusalem.asheber.1@gmail.com>
2025-07-20 13:14:34 +0300

    remove args

--- 2-arguments.js
+++ 2-arguments.js
@@ -6,20 +6,3 @@ if (args.length === 0) {
         else {console.log("Arguments found")
     };
 
-// Get the number of arguments passed (excluding node and script name)
-
-const args1 = [1]
-if (args1.length === 0) {
-    console.log("No argument");}
-    else if (args1.length ===1) {
-        console.log("Argument found")}
-        else {console.log("Arguments found")
-    };
-
-    const args2 = [1,2,3]
-if (args2.length === 0) {
-    console.log("No argument");}
-    else if (args2.length ===1) {
-        console.log("Argument found")}
-        else {console.log("Arguments found")
-    };
