c56e367cbb1e9520a7f4980481fb43cd13094dca
Eyerusalem <eyerusalem.asheber.1@gmail.com>
2025-07-20 13:35:57 +0300

    rm if

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,7 +1,4 @@
 const args = process.argv.slice(2);
 
-console.log(if (args[0] === undefined) {
-  console.log("No argument");
-} else {
-  console.log(args[0]);
-})
+console.log(args[0] === undefined ? "No argument":  args[]
+);
